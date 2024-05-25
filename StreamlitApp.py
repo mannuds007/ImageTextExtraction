3d9550783f03dd423f28788bfe89ff9ecba44352
mannuds007 <123095135+mannuds007@users.noreply.github.com>
2024-05-25 23:38:30 +0530

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -128,7 +128,7 @@ def main():
 
 if __name__ == "__main__":
     # Write credentials to a temporary file
-    credentials_content = st.secrets["GOOGLE_APPLICATION_CREDENTIALS_CONTENT"]['credentials']
+    credentials_content = dict(st.secrets["GOOGLE_APPLICATION_CREDENTIALS_CONTENT"]['credentials'])
     # credentials_json = json.loads(credentials_content)
     # credentials_path = "/tmp/credentials.json"
     # with open(credentials_path, 'w') as f:
